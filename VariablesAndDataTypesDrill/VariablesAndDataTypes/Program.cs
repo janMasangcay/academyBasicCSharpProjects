30ece3b246a8205a201fec4e74f3af64ce9cb37d
JanCaesar <janCaesar@github.com>
2018-08-17 00:22:33 -0700

    Variable and datatypes exercise.

---
+++ VariablesAndDataTypesDrill/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            ushort pageNum = Convert.ToUInt16(page);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            ushort totalHours = Convert.ToUInt16(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
