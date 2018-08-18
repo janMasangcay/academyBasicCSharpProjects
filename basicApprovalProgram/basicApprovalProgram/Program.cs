0babe6e7a71ea882de3b48b1fb431de9f007224c
JanCaesar <janCaesar@github.com>
2018-08-18 16:30:26 -0700

    Basic approval program for car insurance.

---
+++ basicApprovalProgram/basicApprovalProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace basicApprovalProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Form
+            Console.WriteLine("Approval Form");
+            Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticket = Console.ReadLine();
+
+            //Decision
+            Console.WriteLine(Convert.ToInt16(age) > 15 && dui == "false" && Convert.ToInt16(ticket) <= 3);
+            Console.ReadLine();
+        }
+    }
+}
