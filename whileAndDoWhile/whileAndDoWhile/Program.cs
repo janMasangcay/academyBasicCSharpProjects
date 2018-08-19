1076aa4f7110cb5d399e4692dd0aaefbaf2dc168
JanCaesar <janCaesar@github.com>
2018-08-19 02:35:50 -0700

    Made a simple while and do while statement.

---
+++ whileAndDoWhile/whileAndDoWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace whileAndDoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type Erik's lastname to quit!");
+            string name = Console.ReadLine();
+            string myName = "gross";
+
+            while (myName != name)
+            {
+                Console.WriteLine("That is not his lastname.");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Enter Jack's LastName to quit!");
+            string lastName = Console.ReadLine();
+            string myLastName = "stanley";
+
+            do
+            {
+                Console.WriteLine("That is not his LastName.");
+                lastName = Console.ReadLine();
+            }
+            while (myLastName != lastName);
+            Console.WriteLine("Excellent! You did your research.");
+            Console.ReadLine();
+        }
+    }
+}
