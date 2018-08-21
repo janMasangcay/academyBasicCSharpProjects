338b82c0cc9c8d114e4b4ea4c6490cb8c79daf4a
JanCaesar <janCaesar@github.com>
2018-08-21 16:42:04 -0700

    Made a small program using Classes and Methods

---
+++ ClassAndMethod/ClassAndMethodCNFC/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodCNFC
+{
+    class MathOperations
+    {
+        //1.Create a class. In that class, create three methods, each of which will take one integer 
+        //parameter in and return an integer.The methods should do some math operation on the received
+        //parameter.Put this class in a separate.cs file in the application.
+        static public int Addition (int num) 
+        {
+            return num = num + num;
+        }
+        static public int Subtraction (int num)
+        {
+            return num = num - num;
+        }
+        static public int Multiplication (int num)
+        {
+            return num = num * num;
+        }
+    }
+}
---
+++ ClassAndMethod/ClassAndMethodCNFC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodCNFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Write the number you want to use in this operation:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            //3. Call each method in turn, passing the user input to the method.Display the returned integer
+            //to the screen.
+            Console.WriteLine(
+                "Your number added to itself: " + (MathOperations.Addition(userNum)) +
+                "\nYour number subtracted to itself: " + (MathOperations.Subtraction(userNum)) +
+                "\nYour number multiplied to itself: " + (MathOperations.Multiplication(userNum))
+                );
+            Console.ReadLine();
+        }
+    }
+}
