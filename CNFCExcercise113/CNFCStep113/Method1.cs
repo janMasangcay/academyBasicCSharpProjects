5f9f04a947503333585268f54e982807f4f9f0a7
JanCaesar <janCaesar@github.com>
2018-08-21 19:12:42 -0700

    CNFC excercise step113

---
+++ CNFCExcercise113/CNFCStep113/Method1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNFCStep
+{
+    class Method1
+    {
+        //1.Create a class. In that class, create a void method that takes two 
+        //integers as parameters.Have the method do a math operation on the first 
+        //integer and display the second integer to the screen.
+        public void Addition(int num1, int num2)
+        {
+            num1 = num1 + num2;
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CNFCExcercise113/CNFCStep113/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNFCStep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, instantiate the class.
+            Method1 add = new Method1();
+            //3. Call the method in the class, passing in two numbers.
+            add.Addition(12, 21);
+            //4. Call the method in the class, specifying the parameters by name.
+            add.Addition(num1: 12, num2: 21);
+        }
+    }
+}
