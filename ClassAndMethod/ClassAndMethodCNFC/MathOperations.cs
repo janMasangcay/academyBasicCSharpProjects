d2a6fac1be758728b5dcc2e57431eb8eb567e9a5
JanCaesar <janCaesar@github.com>
2018-08-22 02:11:49 -0700

    Made a small program using Classes and Methods

--- ClassAndMethod/ClassAndMethodCNFC/MathOperations.cs
+++ ClassAndMethod/ClassAndMethodCNFC/MathOperations.cs
@@ -10,7 +10,7 @@ namespace ClassAndMethodCNFC
     {
         //1.Create a class. In that class, create three methods, each of which will take one integer 
         //parameter in and return an integer.The methods should do some math operation on the received
-        //parameter.Put this class in a separate.cs file in the application.
+        //parameter. Put this class in a separate.cs file in the application.
         static public int Addition (int num) 
         {
             return num = num + num;
