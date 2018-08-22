3ffd0edc0e39d51fb9126d892c98d2423d5d706b
JanCaesar <janCaesar@github.com>
2018-08-21 19:24:18 -0700

    Class and Methods exercise1.

--- CNFCExcercise113/CNFCStep113/Method1.cs
+++ CNFCExcercise113/CNFCStep113/Method1.cs
@@ -9,7 +9,7 @@ namespace CNFCStep
     class Method1
     {
         //1.Create a class. In that class, create a void method that takes two 
-        //integers as parameters.Have the method do a math operation on the first 
+        //integers as parameters. Have the method do a math operation on the first 
         //integer and display the second integer to the screen.
         public void Addition(int num1, int num2)
         {
