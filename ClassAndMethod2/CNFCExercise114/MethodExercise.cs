816e762f5d49e8e22ee8e05c6b5e721d401c471f
JanCaesar <janCaesar@github.com>
2018-08-21 21:56:57 -0700

    CS course step 114 exercise.

---
+++ ClassAndMethod2/CNFCExercise114/MethodExercise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNFCExercise114
+{
+    class MethodExercise
+    {
+        //1.Create a class. In that class, create a method that will take in an integer, 
+        //do a math operation to it and then return the answer as an integer.
+        public static int Calculate(int num)
+        {
+            return num = num + num;
+        }
+
+        //3. Add a second method to the class, with the same name, that will take in a decimal, 
+        //do a different math operation to it and then return the answer as an integer.
+        public static int Calculate(decimal num)
+        {
+            num = num / 2;
+            return Convert.ToInt32(num);
+        }
+
+        //5. Add a third method to the class, with the same name, that will take in a string, 
+        //convert it to an integer if possible, do a different math operation to it and then 
+        //return the answer as an integer.
+        public static int Calculate(string num)
+        {
+            int convertedNum = Convert.ToInt32(num);
+            return convertedNum = convertedNum * convertedNum;
+        }
+    }
+}
---
+++ ClassAndMethod2/CNFCExercise114/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNFCExercise114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, instantiate the class and call the 
+            //one method, passing in an integer, such as 12. Then display the result to the screen.
+            int add = MethodExercise.Calculate(12);
+            Console.WriteLine("Arg added to itself: {0}", add);
+            Console.ReadLine();
+
+            //4. In the Main() method of the console app, instantiate the class and call the second 
+            //method, passing in a decimal. Display the result to the screen.
+            int divide = MethodExercise.Calculate(12.12m);
+            Console.WriteLine("Arg divide by 2 is: {0}", divide);
+            Console.ReadLine();
+
+            //6. In the Main() method of the console app, instantiate the class and call the third 
+            //method, passing in a string that equates to an integer.Display the result to the screen.
+            int multiply = MethodExercise.Calculate("24");
+            Console.WriteLine("Arg multiplied to itself: {0}", multiply);
+            Console.ReadLine();
+        }
+    }
+}
