d3a1d48b6c31492bb89e02a13b5497d1efc24225
JanCaesar <janCaesar@github.com>
2018-08-22 22:28:57 -0700

    CS course: using Interface.

---
+++ ClassAndMethod7(Interface)/ClassAndMethod/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        //2. Have your Employee class from the previous drill inherit that interface and implement the Quit() 
+        //method in any way you choose.
+        public void Quit()
+        {
+            Console.WriteLine("Resinged employees:");
+            SayName();
+            Console.ReadLine();
+        }
+    }
+}
