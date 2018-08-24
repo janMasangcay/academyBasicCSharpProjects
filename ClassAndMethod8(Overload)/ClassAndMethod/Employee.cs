6eb5c57604a02455950cd0732f97639a1506edc1
JanCaesar <janCaesar@github.com>
2018-08-23 19:05:36 -0700

    CS course: using overloading operator.

---
+++ ClassAndMethod8(Overload)/ClassAndMethod/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Resinged employees:");
+            SayName();
+            Console.ReadLine();
+        }
+
+        //Overload the "==" operator so it checks if two Employee objects are equal by comparing 
+        //their Id property.
+        public static bool operator== (Employee person1, Employee person2)
+        {
+            if (person1.Id == person2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator!= (Employee person1, Employee person2)
+        {
+            if (person1.Id != person2.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ClassAndMethod8(Overload)/ClassAndMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person1 = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student",
+                Id = 1
+            };
+            Employee person2 = new Employee()
+            {
+                firstName = "Beta",
+                lastName = "Student",
+                Id = 2
+            };
+
+            Console.WriteLine(person1 == person2);
+            Console.ReadLine();
+
+            //IQuittable Quit = new Employee() { firstName = "Sample", lastName = "Employee" };
+            //Quit.Quit();
+        }
+    }
+}
