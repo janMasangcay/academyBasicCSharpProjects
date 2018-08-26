f06e9fbbf4c5e9e93912f07a31d1bc439241b3c2
JanCaesar <janCaesar@github.com>
2018-08-25 17:42:37 -0700

    CS course: using Struct.

---
+++ ClassAndMethod11(Struct)/ClassAndMethod/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    public struct Number
+    {
+        //1.Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+        public decimal Abount { get; set; }
+    }
+}
---
+++ ClassAndMethod11(Struct)/ClassAndMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+            Number num = new Number();
+            num.Abount = 12;
+
+            //3. Print this amount to the console.
+            Console.WriteLine(num.Abount);
+            Console.ReadLine();
+        }
+    }
+}
