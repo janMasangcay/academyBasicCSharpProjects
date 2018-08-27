03c37c99d5924ca27439702ae62a47733e9a2ddc
JanCaesar <janCaesar@github.com>
2018-08-27 04:02:37 -0700

    CS course: using file IO.

---
+++ FileIO/File IO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Ask a user for a number.
+            Console.WriteLine("Enter a number");
+            string userInput = Console.ReadLine();
+
+            //2.Log that number to a text file
+            File.WriteAllText(@"C:\Users\jcmas\Desktop\academyProjects\academyProjects\visualStudiosFile\academyBasicCSharpProjects\FileIO\NumberLogs.txt", userInput);
+
+            //3.Print the text file back to the user.
+            userInput = File.ReadAllText(@"C:\Users\jcmas\Desktop\academyProjects\academyProjects\visualStudiosFile\academyBasicCSharpProjects\FileIO\NumberLogs.txt");
+            Console.WriteLine(userInput);
+            Console.ReadLine();
+        }
+    }
+}
