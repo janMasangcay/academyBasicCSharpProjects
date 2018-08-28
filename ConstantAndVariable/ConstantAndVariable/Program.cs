b1b21504fa28efb029b48a88a70ed6316d41bf96
JanCaesar <janCaesar@github.com>
2018-08-27 21:50:37 -0700

    CS course: using const and var.

---
+++ ConstantAndVariable/ConstantAndVariable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantAndVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a const variable
+            const string firstName = "Foo";
+
+            //2.Create a variable using the keyword "var".
+            var lastName = "Bar";
+
+            //3.Chain two constructors together.
+            Console.WriteLine($"{firstName} {lastName}");
+            Console.ReadLine();
+        }
+    }
+}
